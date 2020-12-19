1df245314c821e8a940848efe5912d640f44cb21
Milan Petrovic <milan@gdragon.info>
2020-12-19 11:38:04 +0100

    First files added.

---
+++ gdrts-custom-rating-calculation.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+Plugin Name:       GD Rating System Pro: Custom Rating Calculation
+Plugin URI:        https://rating.dev4press.com/
+Description:       This is a demo plugin showing how to add a custom rating value (other than just average rating) and use it for ordering and display.
+Author:            Milan Petrovic
+Author URI:        https://www.dev4press.com/
+Text Domain:       gdrts-custom-rating-calculation
+Version:           1.0
+Requires at least: 5.0
+Tested up to:      5.6
+Requires PHP:      7.0
+License:           GPLv3 or later
+License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+
+== Copyright ==
+Copyright 2008 - 2020 Milan Petrovic (email: milan@gdragon.info)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
+
---
+++ readme.md
@@ -0,0 +1,3 @@
+# GD Rating System Pro: Custom Rating Calculation
+
+This is a demo plugin showing how to add a custom rating value (other than just average rating) and use it for ordering and display.
